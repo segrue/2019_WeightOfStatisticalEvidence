cbe6b783a95d489227f1d404e2f915b794808d58
segrue <servan.grueninger@gmail.com>
2019-05-17 15:40:06 +0200

    Add backup for report

--- 3_Code/CodeMasterThesis/scripts/PublicationBias.R
+++ 3_Code/CodeMasterThesis/scripts/PublicationBias.R
@@ -3,6 +3,10 @@
 require("reshape2") #https://stackoverflow.com/questions/21563864/ggplot2-overlay-density-plots-r
 require("data.table")
 
+#for plotting
+require("ggplot2")
+require("gridExtra")
+
 #custom functions
 source("./functions/helper_functions.R")
 
@@ -14,7 +18,7 @@ source("./functions/helper_functions.R")
 seed <- 20190514
 set.seed(seed)
 
-n_sim <- 1e3 #number of times the setting below shall be simulated
+n_sim <- 1e2 #number of times the setting below shall be simulated
 n_studies <- c(5,10,20,30,40,50,100,200,500,1000) #number of subjects per study
 
 mu0s <- 0
@@ -33,9 +37,25 @@ load(paste0("data/",name,".RData"),verbose=TRUE) #load data set containing simul
 ### simulate publication bias
 ## Situtation 1:
 ## no publication bias - all studies (n=100) are included
-dat <- evidence_df[mu1=="0.2",]
-p1 <- ggplot(data=dat,aes(x=Tn/sqrt(dat$n_study),y=),col=id,group=interaction(p0,id),linetype=factor(p0))) + 
-  geom_line() + scale_color_manual(values=c("clt"=3,"vst"=4,"std_norm"=2))+
+clt <- evidence_df[mu1=="0.2" & id == "clt",]
+vst <- evidence_df[mu1=="0.2" & id == "vst",]
+stud <- evidence_df[mu1=="0.2" & id == "stud",]
+p1 <- ggplot(data=clt,aes(x=Tn/sqrt(dat$n_study),y=n_study,col=factor(n_study))) + geom_point() + xlim(-3,3)
+p2 <- ggplot(data=clt,aes(x=Tn/sqrt(dat$n_study),y=1/(sgm_hat/sqrt(dat$n_study)),col=factor(n_study))) + geom_point()+ xlim(-3,3)
+p3 <- ggplot(data=stud,aes(x=Tn/sqrt(dat$n_study),y=n_study,col=factor(n_study))) + geom_point() + xlim(-3,3)
+p4 <- ggplot(data=stud,aes(x=Tn/sqrt(dat$n_study),y=1/(sgm_hat/sqrt(dat$n_study)),col=factor(n_study))) + geom_point()+ xlim(-3,3)
+p5 <- ggplot(data=vst,aes(x=Tn/sqrt(dat$n_study),y=n_study,col=factor(n_study))) + geom_point() + xlim(-3,3)
+p6 <- ggplot(data=vst,aes(x=Tn/sqrt(dat$n_study),y=1/(sgm_hat/sqrt(dat$n_study)),col=factor(n_study))) + geom_point()+ xlim(-3,3)
+p7 <- ggplot(data=clt,aes(x=mu1_hat,y=n_study,col=factor(n_study))) + geom_point()+ xlim(-3,3)
+p8 <- ggplot(data=clt,aes(x=mu1_hat,y=1/(sgm_hat/sqrt(dat$n_study)),col=factor(n_study))) + geom_point()+ xlim(-3,3)
+
+grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,nrow=4,ncol=2)
+
+## Situtation 1:
+## only keep studies which turned out to be significant
+clt_sig <- clt[]
+  
+geom_line() + scale_color_manual(values=c("clt"=3,"vst"=4,"std_norm"=2))+
   ylim(-3,3) + xlim(0,1) + ggtitle(bquote("Study size"==.(dat$n_study[1]))) + labs(x= expression(p[1]), y = "Evidence/sqrt(n)")
 return(T_plot)
 
